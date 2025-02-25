bc3f79a6e4aa4425b3be2858c0d29e32de854f5e
Erik Abair <abaire@users.noreply.github.com>
2025-02-24 21:03:56 -0800

    Fixes emulator command generation on Windows.

--- src/nxdk_pgraph_test_runner/config.py
+++ src/nxdk_pgraph_test_runner/config.py
@@ -85,7 +85,8 @@ class Config:
         if not self._emulator_command:
             msg = "Emulator command was not provided."
             raise ValueError(msg)
-        return shlex.split(self._emulator_command.replace("{ISO}", iso_path))
+        posix = platform.system() != "Windows"
+        return shlex.split(self._emulator_command.replace("{ISO}", iso_path), posix=posix)
 
     def set_emulator_command(self, template: str):
         self._emulator_command = template
